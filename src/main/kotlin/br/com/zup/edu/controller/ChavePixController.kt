daf70fcf556586386975a3b81847c6cd3f7680a0
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-24 09:12:20 -0300

    Expondo para o frontend: Remoção de uma chave Pix existente

--- src/main/kotlin/br/com/zup/edu/controller/ChavePixController.kt
+++ src/main/kotlin/br/com/zup/edu/controller/ChavePixController.kt
@@ -1,6 +1,7 @@
 package br.com.zup.edu.controller
 
 import br.com.zup.edu.ChavePixRequest
+import br.com.zup.edu.IdPixRequest
 import br.com.zup.edu.KeyManagerGrpcServiceGrpc
 import br.com.zup.edu.dto.ChavePixDTO
 import io.grpc.Status
@@ -8,18 +9,19 @@ import io.grpc.StatusRuntimeException
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Post
 import io.micronaut.validation.Validated
 import jakarta.inject.Inject
 import javax.validation.Valid
 
 @Validated
-@Controller("/api/cliente/{identificador}")
+@Controller("/api/cliente/{identificador}/chave-pix")
 class ChavePixController(
     @Inject val grpcClient: KeyManagerGrpcServiceGrpc.KeyManagerGrpcServiceBlockingStub
 ) {
 
-    @Post("/chave-pix")
+    @Post
     fun registraChave(identificador: String ,@Body @Valid chavePix: ChavePixDTO): HttpResponse<Any> {
 
         val request = ChavePixRequest.newBuilder()
@@ -33,6 +35,20 @@ class ChavePixController(
 
         val uri = HttpResponse.uri("/api/cliente/$identificador/chave-pix/${response.pixId}")
         return HttpResponse.created( uri )
+
+    }
+
+    @Delete("/{pixId}")
+    fun deletaChave(identificador: String, pixId: Long): HttpResponse<Any> {
+
+        val request = IdPixRequest.newBuilder()
+            .setPixId(pixId)
+            .setIdentificador(identificador)
+            .build()
+
+        grpcClient.deletaChavePix(request)
+
+        return HttpResponse.ok()
 
     }
 
